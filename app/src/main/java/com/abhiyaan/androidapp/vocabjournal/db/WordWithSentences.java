d45996f4544c0ff2eadbae593803e28f203d0160
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-26 18:14:41 -0700

    create WordWithSentences

---
+++ app/src/main/java/com/abhiyaan/androidapp/vocabjournal/db/WordWithSentences.java
@@ -0,0 +1,35 @@
+package com.abhiyaan.androidapp.vocabjournal.db;
+
+import android.arch.persistence.room.Embedded;
+import android.arch.persistence.room.Relation;
+
+import java.util.List;
+
+/**
+ * Created by Binaya Bhattarai on 3/26/2018.
+ */
+
+public class WordWithSentences {
+
+    @Embedded
+    Word word;
+
+    @Relation(parentColumn = "title", entityColumn = "word_id", entity = Sentence.class)
+    List<Sentence> sentences;
+
+    public Word getWord() {
+        return word;
+    }
+
+    public void setWord(Word word) {
+        this.word = word;
+    }
+
+    public List<Sentence> getSentences() {
+        return sentences;
+    }
+
+    public void setSentences(List<Sentence> sentences) {
+        this.sentences = sentences;
+    }
+}
