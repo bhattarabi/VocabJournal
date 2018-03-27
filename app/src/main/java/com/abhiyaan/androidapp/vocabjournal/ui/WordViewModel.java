ad4214aeefbffd45d39edee445372f2376af26ff
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-27 09:08:10 -0700

    add AppRepository field

--- app/src/main/java/com/abhiyaan/androidapp/vocabjournal/ui/WordViewModel.java
+++ app/src/main/java/com/abhiyaan/androidapp/vocabjournal/ui/WordViewModel.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
 
+import com.abhiyaan.androidapp.vocabjournal.AppRepository;
 import com.abhiyaan.androidapp.vocabjournal.db.Sentence;
 import com.abhiyaan.androidapp.vocabjournal.db.Word;
 import com.abhiyaan.androidapp.vocabjournal.db.WordWithSentences;
@@ -21,8 +22,13 @@ public class WordViewModel extends AndroidViewModel{
 
     private LiveData<List<Sentence>> sentences;
 
+    private AppRepository appRepository;
+
     public void init(WordWithSentences wordWithSentences){
+        appRepository = new AppRepository();
         //TODO
+        //setup currentWord
+        //setup sentences
     }
 
     public WordViewModel(@NonNull Application application) {
