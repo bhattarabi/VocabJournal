e14afa65839b9c1e0dba36931962914f6fd0e3f3
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-27 08:47:16 -0700

    finish AppWebInterface implementation

---
+++ app/src/main/java/com/abhiyaan/androidapp/vocabjournal/AppWebInterface.java
@@ -0,0 +1,45 @@
+package com.abhiyaan.androidapp.vocabjournal;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+
+import com.abhiyaan.androidapp.vocabjournal.db.Word;
+import com.abhiyaan.androidapp.vocabjournal.utils.HttpUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Binaya Bhattarai on 3/27/2018.
+ */
+
+public class AppWebInterface {
+
+    HttpUtils httpUtils = new HttpUtils();
+
+    public LiveData<Word> getDefinition(String word){
+        final MutableLiveData<Word> newWord = new MutableLiveData<>();
+        newWord.setValue(getDefinitionFromWeb(word));
+        return newWord;
+    }
+
+
+    private Word getDefinitionFromWeb(String wordTitle) {
+
+        HttpsURLConnection urlConn = httpUtils.makeUrlConnection(wordTitle);
+
+        String jsonResponse = httpUtils.readFromUrlConnection(
+                httpUtils.makeUrlConnection(wordTitle));
+
+        ArrayList<LexicalEntry> defs =
+                httpUtils.getDictionaryEntriesFromJson(jsonResponse);
+
+        String definition = "";
+        for (LexicalEntry l: defs)
+            definition += l+"\n\n";
+
+        return new Word(wordTitle, definition, new Date());
+    }
+}
