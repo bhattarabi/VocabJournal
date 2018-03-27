d54a5c48db02106da58fd70e13d57acf9d2e953f
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-26 19:32:13 -0700

    create AppDatabase

---
+++ app/src/main/java/com/abhiyaan/androidapp/vocabjournal/db/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.abhiyaan.androidapp.vocabjournal.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/**
+ * Created by Binaya Bhattarai on 3/23/2018.
+ */
+@Database(entities = {Word.class, Sentence.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract WordDao wordDao();
+
+    public abstract SentenceDao sentenceDao();
+
+    public static AppDatabase getInMemoryDatabase(Context context){
+        if (INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(
+                    context, AppDatabase.class, "vocab_journal.db")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return INSTANCE;
+    }
+}
