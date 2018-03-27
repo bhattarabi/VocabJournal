310d3dd5be98ce1eea804fcfc64d5f6f2fa74812
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-27 07:41:36 -0700

    create outline for WordViewModel

---
+++ app/src/main/java/com/abhiyaan/androidapp/vocabjournal/ui/WordViewModel.java
@@ -0,0 +1,31 @@
+package com.abhiyaan.androidapp.vocabjournal.ui;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.abhiyaan.androidapp.vocabjournal.db.Sentence;
+import com.abhiyaan.androidapp.vocabjournal.db.Word;
+import com.abhiyaan.androidapp.vocabjournal.db.WordWithSentences;
+
+import java.util.List;
+
+/**
+ * Created by Binaya Bhattarai on 3/27/2018.
+ */
+
+public class WordViewModel extends AndroidViewModel{
+
+    private LiveData<Word> currentWord;
+
+    private LiveData<List<Sentence>> sentences;
+
+    public void init(WordWithSentences wordWithSentences){
+        //TODO
+    }
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
