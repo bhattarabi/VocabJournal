b34c59ef9c124ce17575c4f601a8643bde80c178
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-28 20:48:56 -0700

    organize imports

--- app/src/main/java/com/abhiyaan/androidapp/vocabjournal/MainActivity.java
+++ app/src/main/java/com/abhiyaan/androidapp/vocabjournal/MainActivity.java
@@ -1,11 +1,9 @@
 package com.abhiyaan.androidapp.vocabjournal;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
